3a392c110e2dc32793c8672c9e8efeff51943316
Madhura Jejurkar <jeju6404@stthomas.edu>
2017-09-30 00:28:19 +0000

    script commit

---
+++ jeju6404-Lab02.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+sudo apt-get update
+sudo apt-get install -y tree
+sudo apt-get install -y git
+mkdir -p A/aa A/B/bb A/C/cc A/D/dd
+mkdir -p C/cc C/E/ee C/F/ff
+mkdir -p D/dd D/G/gg D/H/hh D/H/J/jj D/H/K/kk D/I/ii
+# Put commands below
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
